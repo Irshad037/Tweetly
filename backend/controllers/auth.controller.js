ff2eded42592177c7f274b26674e7db5772b634b
Irshad037 <irshad0371234786@gmail.com>
2025-05-25 22:39:58 +0530

    1-project setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+export const signup = async (req, res) => {
+    res.json({
+        "data": "you hit the route signup endpoint"
+    });
+};
+
+export const login = async (req, res) => {
+    res.json({
+        "data": "you hit the route login endpoint"
+    });
+};
+
+export const logout = async (req, res) => {
+    res.json({
+        "data": "you hit the route logout endpoint"
+    });
+};
